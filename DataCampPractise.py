8dea81d5f81b68f5bbd507a87e855971b5b20799
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-10-18 15:41:29 +0300

    Update and rename DataCampPractise to DataCampPractise.py

--- DataCampPractise.py
+++ DataCampPractise.py
@@ -47,5 +47,18 @@ plt.axis("off")
 
 # Don't forget to show the final image
 plt.show()
+#_________________________________________
+# Import the required function
+from sklearn.feature_extraction.text import CountVectorizer
 
+annak = ['Happy families are all alike;', 'every unhappy family is unhappy in its own way']
 
+# Build the vectorizer and fit it
+anna_vect = CountVectorizer()
+anna_vect.fit(annak)
+
+# Create the bow representation
+anna_bow = anna_vect.transfrom(annak)
+
+# Print the bag-of-words result 
+print(anna_bow.toarray())
