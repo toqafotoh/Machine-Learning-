b30bdfc3210ff74d64a58cfe518ce4220950c671
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2023-10-20 18:54:30 +0300

    Update DataCampPractise.py

--- DataCampPractise.py
+++ DataCampPractise.py
@@ -108,3 +108,17 @@ vect2.fit(tweets.negative_reason)
 # Print the last 15 features from the first, and all from second vectorizer
 print(vect1.get_feature_names()[-15:])
 print(vect2.get_feature_names())
+
+
+#__________________________________________________
+# Import the word tokenizing package
+from nltk import word_tokenize 
+
+# Tokenize the text column
+word_tokens = [word_tokenize(review) for review in tweets.text]
+print('Original tokens: ', word_tokens[0])
+
+# Filter out non-letter characters
+cleaned_tokens = [[word for word in item if word.isalpha()] for item in word_tokens]
+print('Cleaned tokens: ', cleaned_tokens[0])
+
